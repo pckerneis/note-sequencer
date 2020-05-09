afdb71cf5fe24ea9d033040ebbe5ff7070a35796
Pierre-Clément Kerneïs <pc.kerneis@gmail.com>
2020-05-09 14:09:39 +0200

    Add intermediary graduations (25% and 75%)

--- src/ts/canvas-components/VelocityRuler.ts
+++ src/ts/canvas-components/VelocityRuler.ts
@@ -12,26 +12,29 @@ export class VelocityRuler extends Component {
     g.fillRect (0, 0, this.width, this.height);
 
     const largeGradW = 10;
-    const smallGradW = 10;
+    const smallGradW = 5;
 
-
+    g.fillStyle = this.model.colors.strokeLight;
 
     // Ruler border
-    g.fillStyle = this.model.colors.strokeDark;
     g.fillRect (this.width - 1, 0, 1, this.height);
 
     // Top grad
-    g.fillStyle = this.model.colors.strokeDark;
     g.fillRect(this.width - largeGradW, 0, largeGradW, 1);
     g.fillStyle = this.model.colors.text;
     g.fillText("100%", this.width - 30, 12);
 
     // Mid grad
-    g.fillStyle = this.model.colors.strokeDark;
-    g.fillRect(this.width - smallGradW, this.height / 2, smallGradW, 1);
+    g.fillRect(this.width - largeGradW, Math.round(this.height * 0.5), largeGradW, 1);
 
     // Bottom grad
     g.fillRect(this.width - largeGradW, this.height - 1, largeGradW, 1);
+
+    // Intermediary grads
+    if (this.height > 60) {
+      g.fillRect(this.width - smallGradW, Math.round(this.height * 0.25), smallGradW, 1);
+      g.fillRect(this.width - smallGradW, Math.round(this.height * 0.75), smallGradW, 1);
+    }
 
     g.fillStyle = this.model.colors.text;
     g.fillText("0", this.width - 20, this.height - 2);
